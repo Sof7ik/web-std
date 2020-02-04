fa17b5c9e65a02cd88a3744c228036a457b997f4
Leonid <bychkov.l47@mail.ru>
2020-02-04 18:17:44 +0300

    Поменял файлики

---
+++ php/autorization.php
@@ -0,0 +1,26 @@
+<?php
+    SESSION_START();
+
+    require_once './connection.php';
+
+    if (isset($_POST['submit']))
+    {
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+
+        $query = Database::query("SELECT login, password FROM `authorization` WHERE login = $login AND password = $password");
+
+        if ($query)
+        {
+            $_SESSION['is_admin'] = 1;
+            // echo $_SESSION['is_admin'];
+            ?>
+            <meta http-equiv="refresh" content="0;../partOfPage.html">
+            <?
+        }
+        else
+        {
+            echo "Такого пользователя не найдено";
+        }
+    }
+?>
