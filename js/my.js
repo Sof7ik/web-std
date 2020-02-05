6fa1c09b9672ac77e719b71e0990ffc5a2be7c27
Alexander <mrreads@yandex.com>
2020-02-05 19:23:24 +0300

    arrow down hide, if screen on header

--- js/my.js
+++ js/my.js
@@ -1,19 +1,38 @@
-var swiperOne = new Swiper('.swiper-container-our', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
+var swiperOne = new Swiper('.swiper-container-our', 
+{
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: 
+  {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
 
-  var swiperTwo = new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+var swiperTwo = new Swiper('.swiper-container', 
+{
+  navigation: 
+  {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
-  var scroll = new SmoothScroll('a[href*="#"]');
+var scroll = new SmoothScroll('a[href*="#"]');
+
+var arrowDown = document.querySelector('.arrow-up');
+var header = document.querySelector('#header');
+
+document.addEventListener('scroll', () =>
+{
+  if (window.scrollY > header.offsetWidth / 2)
+  {
+    arrowDown.style.visibility = 'visible';
+  }
+  else
+  {
+    arrowDown.style.visibility = 'hidden';
+  }
+});
