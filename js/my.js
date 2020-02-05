172bc126a12c53a229d62b32fa644df3231483df
Alexander <mrreads@yandex.com>
2020-02-05 20:16:51 +0300

    TRY TO WRITE BUTTON HIDE UNHIDE MENU

--- js/my.js
+++ js/my.js
@@ -25,6 +25,36 @@ var scroll = new SmoothScroll('a[href*="#"]');
 var arrowDown = document.querySelector('.arrow-up');
 var header = document.querySelector('#header');
 
+var burger  = document.querySelector('.hide-unhide');
+var nav = document.querySelector('nav');
+var navMobile = document.querySelector('.navMobile');
+var closeNavMobile  = document.querySelector('.closeNavMobile');
+
+burger.addEventListener('click', () =>
+{
+  if (nav.style.display == 'flex')
+  {
+    nav.style.display = 'none';
+    navMobile.style.display = 'flex';
+    closeNavMobile.style.display = 'none';
+    document.body.style.overflow = 'scroll';
+  }
+  else
+  {
+    closeNavMobile.style.display = 'flex';
+    nav.style.display = 'flex';
+    navMobile.style.display = 'none';
+    document.body.style.overflow = 'hidden';
+  }
+});
+
+closeNavMobile.addEventListener('click', () =>
+{
+  nav.style.display = 'none';
+  navMobile.style.display = 'flex';
+  closeNavMobile.style.display = 'none';
+});
+
 document.addEventListener('scroll', () =>
 {
   if (window.scrollY > header.offsetWidth / 2)
@@ -34,5 +64,30 @@ document.addEventListener('scroll', () =>
   else
   {
     arrowDown.style.visibility = 'hidden';
+  }
+});
+
+window.addEventListener('resize', () =>
+{
+  if (window.innerWidth >= 875)
+  {
+    closeNavMobile.style.display = 'none';
+  }
+  else
+  {
+    if (nav.style.display == 'flex')
+    {
+      nav.style.display = 'none';
+      navMobile.style.display = 'flex';
+      closeNavMobile.style.display = 'none';
+      document.body.style.overflow = 'scroll';
+    }
+    else
+    {
+      document.body.style.overflow = 'hidden';
+      closeNavMobile.style.display = 'flex';
+      nav.style.display = 'flex';
+      navMobile.style.display = 'none';
+    }
   }
 });
