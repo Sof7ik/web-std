4879d0f763d8014e7289419bb0463a5d78e6d8d7
Sof7ik <bychkov.l47@mail.ru>
2020-02-05 14:05:42 +0300

    Начата админка

--- admin.php
+++ admin.php
@@ -9,22 +9,42 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="./php/adding_news.php" method="POST" class="new-news-wrapper" enctype="multipart/form-data">
-        <img src="img/icons/admin/close-news.png" alt="close-news" class="close-news">
+    <div class="wrapper">
+        <div class="left-container">
+            <div class="edit-studio-title">
 
-        <h1 class="new-news-title">Заголовок</h1>
-        <input type="text" class="input-title" placeholder="Введите заголовок..." name="title_news" required>
+                <span>ВВЕДИТЕ НОВОЕ НАЗВАНИЕ</span>
+                <input type="text" class="edit-studio-title" name="edit-studio-title">
 
-        <h1 class="new-news-title">Содержание</h1>
-        <div class="new-news-instuments">
-            <img src="img/icons/admin/trash.png" alt="trash" class="instuments-icons">
-            <img src="img/icons/admin/add-image.png" alt="add" class="instuments-icons">
-            <input type="file" name="image_news" accept=".png, .jpg, .jpeg">
+                <span>Текущее название</span>
+                <input type="text" class="current-studio-title" value="">
+                
+            </div>
+
+            <form action="./php/adding_news.php" method="POST" class="new-news-wrapper" enctype="multipart/form-data">
+                <img src="img/icons/admin/close-news.png" alt="close-news" class="close-news">
+
+                <h1 class="new-news-title">Заголовок</h1>
+                <input type="text" class="input-title" placeholder="Введите заголовок..." name="title_news" required>
+
+                <h1 class="new-news-title">Содержание</h1>
+                <div class="new-news-instuments">
+                    <img src="img/icons/admin/trash.png" alt="trash" class="instuments-icons">
+                    <img src="img/icons/admin/add-image.png" alt="add" class="instuments-icons">
+                    <input type="file" name="image_news" accept=".png, .jpg, .jpeg">
+                </div>
+
+                <textarea class="input-main-text" placeholder="Введите текст..."  name="text_news" required></textarea>
+
+                <input type="submit">
+            </form>
         </div>
 
-        <textarea class="input-main-text" placeholder="Введите текст..."  name="text_news" required></textarea>
+        <div class="right-container">
 
-        <input type="submit">
-    </form>
+        </div>
+    </div>
+    
+    
 </body>
 </html>
